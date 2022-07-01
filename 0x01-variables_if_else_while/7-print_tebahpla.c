607a781756419f286f3b06a42e8377fffe588d78
DerickKinoti <derekkinoti87@gmail.com>
2022-07-01 09:35:15 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * man-Entry point
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch)
+	}
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	} 
+
+	return (0);
+}
+
