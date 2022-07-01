2584e780565850af49db3456276fdf5afdb72d12
DerickKinoti <derekkinoti87@gmail.com>
2022-07-01 05:09:10 -0700

    lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,15 @@
  *
  * Return:Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int ch;
-	for(ch=97;ch<=122;ch++)
+	for (ch = 97; ch <= 122; ch++)
+
 	{
 		putchar(ch);
 	}
+
 	putchar(10);
 
 	return (0);
