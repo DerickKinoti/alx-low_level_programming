2ae24874b114034d05f4dcdf379823a447ed3e96
DerickKinoti <derekkinoti87@gmail.com>
2022-07-05 02:43:34 -0700

    hot fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /*
- *-islower-checks for lowercase letters
- * @c:character arg
- * Returns:1 and 0 depending on alphabet state
+ *_islower-checks for lowercase letters and prints depending on condition
+ *@c:character arg
+ *Returns:1 and 0 depending on alphabet state
  */
 int _islower(int c)
 {
