cd184b10153fe04a52976310825d5646b490cee7
DerickKinoti <derekkinoti87@gmail.com>
2022-07-05 01:50:01 -0700

    4-islpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main-Entry point
+ * @c:character arg
+ * Return:return 1 or 0 based on condition
+ */
+int _isalpha(int c);
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
