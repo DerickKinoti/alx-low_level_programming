aef5a098c4dc12d41f4da20ba0e2a8e93223ae53
DerickKinoti <derekkinoti87@gmail.com>
2022-07-14 03:04:35 -0700

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,10 +2,9 @@ include "main.h"
 
 /**
  * _strcat - Concatenates the string
- * @dest: A pointer to the string
- * @src: The source string 
- *
- * Return: A pointer to the destination string
+ * @dest: A pointer to concanetaed string
+ * @src: Sources string
+ * Return: A pointer to destination string
  */
 char *_strcat(char *dest, char *src)
 {
@@ -13,6 +12,7 @@ char *_strcat(char *dest, char *src)
 
 		while (dest[index++])
 			dest_len++;
+
 		for (index = 0; src[index]; index++)
 			dest[dest_len++] = src[index];
 
